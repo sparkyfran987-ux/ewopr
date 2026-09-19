9406fad8e8f496f7eb15d8f15bb59e39dc3f8ee1
sparkyfran987-ux <fraelvillegasplay9@gmail.com>
2026-09-19 17:20:56 -0400

    Auto-update noticias.ts con 5 publicaciones, links limpios e imágenes

--- src/noticias.ts
+++ src/noticias.ts
@@ -7,37 +7,4 @@ export type Noticia = {
   link: string;
 };
 
-export const noticiasEWO: Noticia[] = [
-  {
-    "id": 1,
-    "titulo": "“Yo se lo que hay esta noche en Toa Baja” | Maximus Winner",
-    "fecha": "EWO 2040",
-    "descripcion": "“Yo se lo que hay esta noche en Toa Baja” | Maximus Winner",
-    "imagen": "/publicacion_fb_1.jpg",
-    "link": "https://www.facebook.com/stories/122095518470292934/UzpfSVNDOjExMjI5MDAzMTM0MjQ5MDI=/"
-  },
-  {
-    "id": 2,
-    "titulo": "¿PAZ o GUERRA? ¿ENEMIGO PÚBLICO? ¡Ahora a las 10:00am por Teleonce! Y en  www.te",
-    "fecha": "EWO 2040",
-    "descripcion": "¿PAZ o GUERRA? ¿ENEMIGO PÚBLICO? ¡Ahora a las 10:00am por Teleonce! Y en  www.teleonce.com",
-    "imagen": "/publicacion_fb_2.jpg",
-    "link": "https://www.facebook.com/ewopuertorico"
-  },
-  {
-    "id": 3,
-    "titulo": "Desde  EWO Puerto Rico  le deseamos el mayor de los éxitos a nuestros compañeros",
-    "fecha": "EWO 2040",
-    "descripcion": "Desde  EWO Puerto Rico  le deseamos el mayor de los éxitos a nuestros compañeros de EWA en su evento Public Enemies: Arreglando Cuentas este sábado en Toa Baja.  Q...  Ver más",
-    "imagen": "/publicacion_fb_3.jpg",
-    "link": "https://www.facebook.com/stories/122095518470292934/UzpfSVNDOjExMjI5MDAzMTM0MjQ5MDI=/"
-  },
-  {
-    "id": 4,
-    "titulo": "𝐋𝐎𝐒 𝐁𝐔𝐄𝐍𝐎𝐒 𝐒𝐎𝐌𝐎𝐒 +   | Hoy reconocemos un gesto que merece ser visto. El luchado",
-    "fecha": "EWO 2040",
-    "descripcion": "𝐋𝐎𝐒 𝐁𝐔𝐄𝐍𝐎𝐒 𝐒𝐎𝐌𝐎𝐒 +   | Hoy reconocemos un gesto que merece ser visto. El luchador puertorriqueño y comentarista de EWO, 𝐄𝐥 𝐖𝐢𝐳𝐚𝐫𝐝, fue captado ayudando a una señora ...  Ver más",
-    "imagen": "/publicacion_fb_4.jpg",
-    "link": "https://www.facebook.com/holapr1"
-  }
-];
+export const noticiasEWO: Noticia[] = [];
