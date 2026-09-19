16d7705779683a89df8855a0ac1e3fd5c5cb7e54
sparkyfran987-ux <fraelvillegasplay9@gmail.com>
2026-09-19 17:11:16 -0400

    Auto-update noticias.ts desde scraper de Facebook

--- src/noticias.ts
+++ src/noticias.ts
@@ -9,48 +9,11 @@ export type Noticia = {
 
 export const noticiasEWO: Noticia[] = [
   {
-    id: 1,
-    titulo: "¡ÚLTIMA HORA",
-    fecha: "EWO 2040",
-    descripcion:
-      "Temprano en la mañana recibimos las siguientes declaraciones por parte del Wizard a solo horas del evento #2040. Estas declaraciones van directas a Kobe La Mascara como tambien a Freddy Rodriguez ya que el Wizard no se guardo ningún comentario y lo dijo TODO.",
-    imagen: "/elwizardfacebook.jpg",
-    link: "https://www.facebook.com/share/v/17X5bdq1NB/",
-  },
-  {
-   id: 1,
-    titulo: "¡EL MISTERIO ESTÁ A PUNTO DE REVELARSE! ¿A QUÉ CAMPEÓN RETARÁ EDWIN GARCÍA?",
-    fecha: "EWO 2040",
-    descripcion:
-      "Su próximo objetivo ya está en la mira, pero la gran pregunta continúa en el aire hasta hoy...",
-    imagen: "/comunicado.png",
-    link: "https://www.facebook.com/share/p/19NpAWJrRk/",
-  },
-  {
-    id: 2,
-    titulo: "¿QUÉ BUSCA EDWIN GARCÍA EN EWO?",
-    fecha: "EWO 2040",
-    descripcion:
-      "En 2014, David Estilo quiso destruir EWO y no pudo. Doce años después regresa como Edwin García, ahora junto a EWA y hablando de… ¿colaboración?",
-    imagen: "/publicacion1.jpg",
-    link: "https://www.facebook.com/share/v/18BiM62Cqn/",
-  },
-  {
-   id: 3,
-    titulo: "¡Los campeones listos para su PRIMERA GRAN PRUEBA!",
-    fecha: "EWO 2040",
-    descripcion:
-      "Peak Puerto Rico, y su Alcalde, Hon. Jesús “Jerry” Márquez  Rodríguez  presentan #EWO2040, el próximo sábado en el Coliseo Ismael Benabe de Luquillo.",
-    imagen: "/publicacion2.jpg",
-    link: "https://www.facebook.com/share/v/1CzFNBLgpj/",
-  },
-  {
-   id: 4,
-    titulo: "𝐕𝐈𝐒𝐈𝐎́𝐍, 𝐂𝐀𝐌𝐁𝐈𝐎𝐒 𝐘 𝐍𝐔𝐄𝐕𝐎𝐒 𝐇𝐎𝐑𝐈𝐙𝐎𝐍𝐓𝐄𝐒…",
-    fecha: "EWO 2040",
-    descripcion:
-      "Llegadas inesperadas, alianzas sorpresivas y movimientos silenciosos comienzan a transformar el panorama de la EWO.",
-    imagen: "/publicacion3.jpg",
-    link: "https://www.facebook.com/share/v/19Yh9iP4im/",
-  },
+    "id": 1,
+    "titulo": "“Yo se lo que hay esta noche en Toa Baja” | Maximus Winner",
+    "fecha": "EWO 2040",
+    "descripcion": "“Yo se lo que hay esta noche en Toa Baja” | Maximus Winner",
+    "imagen": "/default.jpg",
+    "link": ""
+  }
 ];
